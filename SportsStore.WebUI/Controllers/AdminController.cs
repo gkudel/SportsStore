29b0e1068bfb0cd7ef95f360b5c6f656b2ac83eb
Grzegorz Kudlacz <gkudel@gmail.com>
2015-07-20 15:44:15 +0200

    [Add New Product]

--- SportsStore.WebUI/Controllers/AdminController.cs
+++ SportsStore.WebUI/Controllers/AdminController.cs
@@ -43,7 +43,31 @@ namespace SportsStore.WebUI.Controllers
             }
             else
             {
+                /*foreach (ModelState state in ViewData.ModelState.Values.Where(x => x.Errors.Count > 0))
+                {
+                    //state.Errors
+                }*/
                 return View(product);
+            }
+        }
+
+        public ViewResult Create()
+        {
+            return View("Edit");
+        }
+
+        [HttpPost]
+        public ActionResult Create([Bind(Exclude = "ProductID")]Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                repository.SaveProduct(product);
+                TempData["savemessage"] = string.Format("{0} has been saved", product.Name);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("Edit", product);
             }
         }
 	}
